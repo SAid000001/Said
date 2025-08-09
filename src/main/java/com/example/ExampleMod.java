7e465f599317d2c799ba373809fc2f346d8f01a8
SAid000001 <mamatkulovsaid96@gmail.com>
2025-08-09 15:12:13 +0500

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,24 +1,20 @@
-package com.example;
+package com.example.mod;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.player.PlayerEntity;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+public class ModMain implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello, Fabric world!");
+    }
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+    public static void increasePlayerHealth(PlayerEntity player) {
+        EntityAttributeInstance healthAttribute = player.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH);
+        if (healthAttribute != null) {
+            healthAttribute.setBaseValue(40.0D); // 20 сердец = 40 единиц здоровья
+        }
+    }
 }
