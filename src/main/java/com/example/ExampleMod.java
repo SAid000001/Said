36ad7523273dd45d41b20630c7841bf8678c2bee
SAid000001 <mamatkulovsaid96@gmail.com>
2025-08-09 15:43:57 +0500

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,14 +1,22 @@
-plugins {
-    id 'fabric-loom' version '1.+'
-    id 'maven-publish'
+package com.example.mod;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.player.PlayerEntity;
+
+public class ModMain implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello, Fabric world!");
+    }
+
+    public static void increasePlayerHealth(PlayerEntity player) {
+        EntityAttributeInstance healthAttribute = player.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH);
+        if (healthAttribute != null) {
+            healthAttribute.setBaseValue(40.0D); // 20 сердец = 40 единиц здоровья
+        }
+    }
 }
 
-repositories {
-    mavenCentral()
-    maven { url 'https://maven.fabricmc.net/' }
-}
-
-dependencies {
-    implementation 'net.fabricmc:fabric-loader:0.14.+'
-}
 
